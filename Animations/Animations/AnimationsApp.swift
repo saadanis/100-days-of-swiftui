645597065c574785196a64b04531d768f8e3a045
Saad Anis <saadmohammedanis@gmail.com>
2024-05-21 11:35:30 +0300

    Complete Animations project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Saad Anis on 21/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
